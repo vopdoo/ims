9b0e357dfc6011f9cbeac3a9cc4705d7707be1a4
Vopdoo Chen <vopdoo.chen@gmail.com>
2019-04-15 13:35:29 +0800

    Update

--- src/components/ims-layout/ims-layout.vue
+++ src/components/ims-layout/ims-layout.vue
@@ -91,8 +91,8 @@
 </template>
 
 <script>
-    import SiderMenu from '@ims/components/sider-menu/index'
-    import HorizontalTabBar from '@ims/components/horizontal-tab-bar/index'
+    import SiderMenu from '../components/sider-menu/index'
+    import HorizontalTabBar from '../components/horizontal-tab-bar/index'
     // import {mapGetters} from 'vuex'
     // import store from '@ims/store/index';
 
